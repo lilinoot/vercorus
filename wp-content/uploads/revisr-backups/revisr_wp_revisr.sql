c29a203676f292d6077aea16527b7f26345d88ab
VrCs26 <lili.vernet@gmail.com>
2018-05-29 16:01:35 +0000

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wp_revisr.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_revisr.sql
@@ -19,12 +19,12 @@ CREATE TABLE `wp_revisr` (
   `event` varchar(42) NOT NULL,
   `user` varchar(60) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `wp_revisr` WRITE;
 /*!40000 ALTER TABLE `wp_revisr` DISABLE KEYS */;
-INSERT INTO `wp_revisr` VALUES (1,'2018-05-29 17:50:55','Successfully created a new repository.','init','VrCs26');
+INSERT INTO `wp_revisr` VALUES (1,'2018-05-29 17:50:55','Successfully created a new repository.','init','VrCs26'),(2,'2018-05-29 17:51:44','Successfully backed up the database.','backup','VrCs26'),(3,'2018-05-29 17:51:51','Committed <a href=\"http://ets-vercorus.com/wp-admin/admin.php?page=revisr_view_commit&commit=94ccc83&success=true\">#94ccc83</a> to the local repository.','commit','VrCs26'),(4,'2018-05-29 17:51:51','Error pushing changes to the remote repository.','error','VrCs26'),(5,'2018-05-29 17:52:21','Error pushing changes to the remote repository.','error','VrCs26'),(6,'2018-05-29 17:58:21','Successfully pushed 2 commits to origin/master.','push','VrCs26');
 /*!40000 ALTER TABLE `wp_revisr` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
