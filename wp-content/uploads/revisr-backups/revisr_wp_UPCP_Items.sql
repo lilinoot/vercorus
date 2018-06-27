41fadbcf68e2447686158e3318b7f7744180097d
lilinoot <lili.vernet@gmail.com>
2018-06-27 12:54:04 +0000

    Backed up the database with Revisr.

---
+++ wp-content/uploads/revisr-backups/revisr_wp_UPCP_Items.sql
@@ -0,0 +1,57 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `wp_UPCP_Items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wp_UPCP_Items` (
+  `Item_ID` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `Item_Name` text NOT NULL,
+  `Item_Slug` text NOT NULL,
+  `Item_Description` text,
+  `Item_Price` text NOT NULL,
+  `Item_Sale_Price` text NOT NULL,
+  `Item_Sale_Mode` text NOT NULL,
+  `Item_Link` text,
+  `Item_Photo_URL` text,
+  `Category_ID` mediumint(9) DEFAULT '0',
+  `Category_Name` text,
+  `Global_Item_ID` mediumint(9) DEFAULT '0',
+  `Item_Special_Attr` text,
+  `SubCategory_ID` mediumint(9) DEFAULT '0',
+  `SubCategory_Name` text,
+  `Item_Date_Created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `Item_Views` mediumint(9) DEFAULT '0',
+  `Item_Display_Status` text,
+  `Item_Related_Products` text,
+  `Item_Next_Previous` text,
+  `Item_SEO_Description` text,
+  `Item_Category_Product_Order` mediumint(9) DEFAULT '9999',
+  `Item_WC_ID` mediumint(9) DEFAULT '0',
+  UNIQUE KEY `id` (`Item_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+LOCK TABLES `wp_UPCP_Items` WRITE;
+/*!40000 ALTER TABLE `wp_UPCP_Items` DISABLE KEYS */;
+INSERT INTO `wp_UPCP_Items` VALUES (1,'Bain norvégien','bain-norvégien','<p>This is where your description of this product would go.</p>\n','','','No','','http://ets-vercorus.com/wp-content/uploads/2018/06/DSC2288.jpg',1,'Sample Category',0,'',0,'','0000-00-00 00:00:00',1,'Show','','','',9999,0);
+/*!40000 ALTER TABLE `wp_UPCP_Items` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
